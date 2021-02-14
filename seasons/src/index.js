4f5b98d9438ad2e2222808864eb63452de012602
cveigt <cveigt@gmail.com>
2021-02-13 23:56:59 -0800

    Add componentDidMount method
    Refactored Constructor function and added componentDidMount method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,18 +8,12 @@ class App extends React.Component {
     // THIS IS THE ONLY TIME we do direct assignment
     // to this.state
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState!!!!
-        this.setState({ lat: position.coords.latitude });
-
-        // we did not!!!
-        //this.state.lat = position.coords.latitude;
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
